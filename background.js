513d9779a4cb7896d049e397508845d49d22ba31
BMamba2942 <jmiddleton9140@sbcglobal.net>
2019-10-29 17:48:09 -0500

    corrected issue with cm item
    correct issue where cm item would only react to clicks on install

--- background.js
+++ background.js
@@ -5,10 +5,11 @@ setupContextMenu = () => {
 		title: 'Identify Rookie'
 	});
 
-	chrome.contextMenus.onClicked.addListener((info, tab) => {
-		chrome.tabs.executeScript(tab.id, { file: 'rookie-id.js'});
-	});
 };
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	chrome.tabs.executeScript(tab.id, { file: 'rookie-id.js'});
+});
 
 chrome.runtime.onInstalled.addListener(function() {
 	setupContextMenu();
