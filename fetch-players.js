eb51364e5aff430b052e56a170fee749c4d10071
BMamba2942 <jmiddleton9140@sbcglobal.net>
2022-01-22 13:37:33 -0600

    a little refactoring and added downloading csv

--- fetch-players.js
+++ fetch-players.js
@@ -1,31 +1,8 @@
-let currentSearch = window.location.search;
-let canSearch = true;
-const max = currentSearch.match(/max=([0-9]+)/);
-if (!max) {
-  currentSearch += 'max=500';
-}
-else if (max[1] !== '500') {
-  currentSearch.replace(/max=[0-9]+/, `max=500`)
-}
-seasonSelected = document.getElementById('season').value;
-season2Selected = document.getElementById('season2').value;
-if (seasonSelected !== season2Selected || seasonSelected === 'Any Season') {
-  alert('Multi-season is not supported at this time. Please set seasons to same year');
-  chrome.storage.local.set({isSearching: false});
-  canSearch = false;
-}
-if (!seasonSelected || seasonSelected === 'Any Season') {
-  alert('Please select a single season before continuing');
-  chrome.storage.local.set({isSearching: false});
-  canSearch = false;
-}
-if (canSearch) {
+if (canSearch()) {
   var matches = Array.from(document.getElementsByClassName('wis_centeredContent'));
   var matchCount = matches.filter(match => match.innerText.includes(' matches were found'))[0];
-  if (!matchCount) {
-    chrome.storage.local.set({isSearching: true});
-    document.getElementById('max').value = '500';
-    document.getElementById('cmdSearch').click();
+  if (!matchCount || !getNextButton()) {
+    beginSearch();
   }
   else {
     matchCount = matchCount.innerText.match(/([0-9]+) matches were found.*/)[1];
@@ -44,22 +21,65 @@ if (canSearch) {
         allNames = [];
       }
       const playerNames = Array.from(document.querySelectorAll('td.wis_left > a:not(.RefLink)')).map(row => row.innerText.trim());
-      allNames = allNames.concat(playerNames);
-      if (!allNames || allNames.length == 0)
+      if (!playerNames || playerNames.length === 0)
       {
-        console.error('no names parsed')
+        console.error('No names parsed')
       }
-      matchCount -= allNames.length;
-      if (matchCount > 0 && allNames.length > 0) {
-        let currentPage = Number.parseInt(currentSearch.match(/pg=([0-9]{1,2})/)[1]);
-        const nextSearch = currentSearch.replace(/pg=[0-9]{1,2}/, `pg=${++currentPage}`);
+      const pageOfNames = new Set();
+      playerNames.map(playerName => playerName.split(',').reverse().join(' '))
+        .forEach(playerName => pageOfNames.add(playerName.trim()));
+      matchCount -= playerNames.length;
+      allNames = allNames.concat(Array.from(pageOfNames));
+      const nextPage = getNextButton();
+      if (nextPage) {
         chrome.storage.local.set({allNames, isSearching: true});
-        setTimeout(() => window.location.search = nextSearch, 4000);
+        setTimeout(() => nextPage.click(), 4000);
       }
       else {
-        console.log('make file!', allNames)
+        makeCsvFile(allNames);
         chrome.storage.local.remove(['allNames', 'isSearching']);
       }
     })
   }
+}
+
+function canSearch() {
+  let canSearch = true;
+  seasonSelected = document.getElementById('season').value;
+  season2Selected = document.getElementById('season2').value;
+  if (seasonSelected !== season2Selected || seasonSelected === 'Any Season') {
+    alert('Multi-season is not supported at this time. Please set seasons to same year');
+    chrome.storage.local.remove(['allNames', 'isSearching']);
+    canSearch = false;
+  }
+  if (!seasonSelected || seasonSelected === 'Any Season') {
+    alert('Please select a single season before continuing');
+    chrome.storage.local.remove(['allNames', 'isSearching']);
+    canSearch = false;
+  }
+
+  return canSearch;
+};
+
+function beginSearch()  {
+  chrome.storage.local.set({isSearching: true});
+  document.getElementById('max').value = '500';
+  document.getElementById('sb').value = '1'; // 'Name'
+  document.querySelector('input[type="radio"][name="order"][value="1"]').checked = true; // 'Ascending' order
+  document.getElementById('cmdSearch').click();
+}
+
+function getNextButton() {
+  return document.querySelector('a.wis_buttonSecondary[title="Next"]');
+};
+
+function makeCsvFile(playerData) {
+  if (!playerData || playerData.length === 0) {
+    console.error('No players to create file from!');
+    return;
+  }
+
+  const csvContent = 'data:text/csv;charset=utf-8,' + playerData.join('\n');
+  const encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
 }
